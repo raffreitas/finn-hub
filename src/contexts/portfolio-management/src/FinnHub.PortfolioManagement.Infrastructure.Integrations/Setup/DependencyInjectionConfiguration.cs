29e159a22ec11a94631353b52d87ea3041a6d0b5
Rafael Freitas <raffreitasx@outlook.com>
2025-06-26 19:07:10 -0300

    feat(integrations): add market data service
    - Updated `docker-compose.yaml` to include health checks for PostgreSQL, MongoDB, and Redis services, and added a new Redis service configuration.
    - Enhanced `CreateAssetEndpoint.cs` to produce a 400 Bad Request response and tagged it with "Assets".
    - Introduced `GetLatestPriceEndpoint` for retrieving current asset prices using a quote repository.
    - Created `EndpointTags.cs` to define constants for endpoint tags.
    - Modified solution file to include a new project for `FinnHub.PortfolioManagement.Infrastructure.Integrations`.
    - Added `IMarketDataService` and `ITokenService` interfaces for market data retrieval and token management.
    - Updated `RegisterBuyAssetHandler.cs` to depend on `IMarketDataService` for fetching current market values.
    - Implemented `TokenService.cs` for JWT token generation and caching.
    - Updated `DependencyInjectionConfiguration.cs` to register new services for caching and market data integration.
    - Implemented `MarketDataService.cs` to interact with an external market data API.
    - Updated `appsettings.Development.json` and `appsettings.Test.json` with new caching and market data settings.
    - Modified `WebApiFactory.cs` to include Redis container setup for testing.
    - Updated `PortfolioControllerTests.cs` to use mocks for market data service interactions.
    - Added necessary package references for NSubstitute and Testcontainers in project files.

---
+++ src/contexts/portfolio-management/src/FinnHub.PortfolioManagement.Infrastructure.Integrations/Setup/DependencyInjectionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using FinnHub.PortfolioManagement.Infrastructure.Integrations.Services.MarketData.Setup;
+using FinnHub.PortfolioManagement.Infrastructure.Integrations.Shared.Policies;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Polly;
+
+namespace FinnHub.PortfolioManagement.Infrastructure.Integrations.Setup;
+public static class DependencyInjectionConfiguration
+{
+    public static IServiceCollection AddIntegrationsConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMarketDataConfiguration(configuration);
+
+        services.AddSingleton<IPolicyFactory<IAsyncPolicy<HttpResponseMessage>>, PolicyFactory>();
+        return services;
+    }
+}
